11dce7ea4b1b25d9178438c3dfd84f21409a833f
Raiden Dilan <raiden18@me.com>
2017-02-10 16:33:34 +0000

    created html and css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
 console.log('JS loaded!');
+// $(() => {
+
+// Show instructions when the key 'I' is pressed and held
+
+// Reset the board if the player runs into a wall within the board. Or don't do this at all.
+
+// Create the walls within the board
+
+// Create the direction key function
+
+// Create the 'player' function
+
+// Create the walls of the edges
+
+// Create Player direction function
+
+// Create a 'You Won!' alert/message when the player reaches the finish destination.
+
+// Create
+
+// Cross browser compatibility, if i know how to this.
+
+// Create different difficulties
+
+//
+
+//
+
+
+// CONSOLE LOG EVERYTHING!!!
+// YES! Console everthing
+
+// });
