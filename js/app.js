5bcae255674cb6aec6d9c90f74b693a888b10772
Raiden Dilan <raiden18@me.com>
2017-02-11 21:06:12 +0000

    added play function

--- js/app.js
+++ js/app.js
@@ -1,34 +1,57 @@
 console.log('JS loaded!');
-// $(() => {
+$(() => {
 
 // Show instructions when the key 'I' is pressed and held
 
 // Reset the board if the player runs into a wall within the board. Or don't do this at all.
 
-// Create the walls within the board
+// Create the walls within the board function
 
 // Create the direction key function
+  const $control = $('control');
 
-// Create the 'player' function
+  $control.keypress(function() {
+    console.log( 'Handler for .keypress() called.' );
+  });
 
-// Create the walls of the edges
+// create the play button
+  const $play = $('button.play');
+
+  $play.on('click', () => {
+    // $play.html('started');
+    console.log('Started');
+  });
+
+// Create the reset button
+  const $reset = $('button.reset');
+
+  $reset.on('click', () => {
+    // $ball.prepend('');
+    // $reset.html('started');
+    console.log('Restarted');
+  });
+
+// Create the 'ball' function
+  const $ball = $('.circle');
+
+  $ball.on('click', () => {
+    $ball.appendTo('.destination');
+    console.log('Ball moved');
+  });
+
+// Block the edge so the player can't go through it.
 
 // Create Player direction function
 
 // Create a 'You Won!' alert/message when the player reaches the finish destination.
 
-// Create
-
 // Cross browser compatibility, if i know how to this.
 
 // Create different difficulties
 
-//
-
-//
-
+// Create hide board function
 
 // CONSOLE LOG EVERYTHING!!!
 // YES! Console everthing
 
-// });
+});
