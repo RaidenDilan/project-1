825a690cb514ba980cf3a71d1cac2077ceeadb3d
Raiden Dilan <raiden18@me.com>
2017-02-12 20:06:05 +0000

    changed keydown function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 console.log('JS loaded!');
 $(() => {
 
-// Show instructions when the key 'I' is pressed and held
+// Show instructions when the key 'I' is pressed and held /////////////////////
 
-// Reset the board if the player runs into a wall within the board. Or don't do this at all.
+// Reset the board if the player runs into a wall within the board ////////////
 
-// Create the walls within the board function
+// Create the walls within the board function//////////////////////////////////
 
-// add the timer
+// add the timer //////////////////////////////////////////////////////////////
   const $input = $('input');
   // const $button = $('.submit');
   const $timerScreen = $('.timer');
@@ -17,16 +17,16 @@ $(() => {
   let timerId = null;
 
   function startStopTimer() {
-// show button input
+// Show button input
     showInput();
 
-// starts the timer
+// Starts the timer
     timerId = setInterval(() => {
       timeRemaining--;
       $timerScreen.text(timeRemaining);
       console.log('Set Interval Working!');
 
-      // stops the timer
+// Stops the timer
       if(timeRemaining === 0) {
         clearInterval(timerId);
         $display.html('Game Over');
@@ -37,56 +37,104 @@ $(() => {
     }, 1000);
 // timerIsRunning = true;
   }
-// hides input box
+// Hides input box ////////////////////////////////////////////////////////////
   function hideInput() {
     $input.hide();
   }
   hideInput();
 
-// shows input box
+// Shows input box
   function showInput() {
     $input.show();
   }
 // runs the GO! button
   $startBtn.on('click', startStopTimer);
 
-// Create the direction key function
-  setInterval(moveBall, 50); //50 marks the speed of the ball
-  const keys = {};
+// Create the direction key function //////////////////////////////////////////
+
+  const $circle = $('.circle'); //the circle object
 
   $(document).keydown(function(e) {
-    keys[e.keyCode] = true;
-  });
-
-  $(document).keyup(function(e) {
-    delete keys[e.keyCode];
-  });
-
-  function moveBall() {
-    for (var direction in keys) {
-      if (!keys.hasOwnProperty(direction)) continue;
-      if (direction == 37) {
-        $ball.animate({left: '-=5'}, 0); //left works fine
-        console.log('Left');
-      }
-      if (direction == 38) {
-        $ball.animate({top: '-=5'}, 0); //top works fine
-        console.log('Top');
-      }
-      if (direction == 39) {
-        $ball.animate({right: '+=5'}, 0); //right still not working
-        console.log('Right');
-      }
-      if (direction == 40) {
-        $ball.animate({down: '+=5'}, 0); //down stil not working
-        console.log('Down');
-      }
+    switch (e.which) {
+      case 37:
+        $circle.stop().css({
+          left: '-=10' //change number to set the amount of pixels the ball moves
+        }); //left arrow key
+        break;
+      case 38:
+        $circle.stop().css({
+          top: '-=10' //change number to set the amount of pixels the ball moves
+        }); //up arrow key
+        break;
+      case 39:
+        $circle.stop().css({
+          left: '+=10' //change number to set the amount of pixels the ball moves
+        }); //right arrow key
+        break;
+      case 40:
+        $circle.stop().css({
+          top: '+=10' //change number to set the amount of pixels the ball moves
+        }); //bottom arrow key
+        break;
     }
-  }
+  });
 
-// move the ball
+  // function moveBall(e) {
+  //   switch(e.keyCode) {
+  //     case 37:
+  //     // animate ball left
+  //       break;
+  //     case 38:
+  //     // animate ball up
+  //       break;
+  //     case 39:
+  //     // animate ball right
+  //       break;
+  //     case 40:
+  //     // animate ball down
+  //       break;
+  //   }
+  //   console.log('ball motion working');
+  // }
+  // moveBall();
 
-// create the play button
+  // setInterval(moveBall, 50); //50 marks the speed of the ball
+  // const keys = {};
+  //
+  // $(document).keydown(function(e) {
+  //   keys[e.keyCode] = true;
+  // });
+  //
+  // $(document).keyup(function(e) {
+  //   delete keys[e.keyCode];
+  // });
+  //
+  // function moveBall() {
+  //   for (var direction in keys) {
+  //     if (!keys.hasOwnProperty(direction)) continue;
+  //     if (direction == 37) {
+  //       $ball.css({left: '-=5'}, 100); //left works fine
+  //       console.log('Left');
+  //     }
+  //     if (direction == 38) {
+  //       $ball.css({top: '-=5'}, 100); //top works fine
+  //       console.log('Top');
+  //     }
+  //     if (direction == 39) {
+  //       $ball.css({right: '+=5'}, 100); //right still not working
+  //       console.log('Right');
+  //     }
+  //     if (direction == 40) {
+  //       $ball.css({down: '+=5'}, 100); //down stil not working
+  //       console.log('Down');
+  //     }
+  //   }
+  // }
+
+// Move the ball //////////////////////////////////////////////////////////////
+
+
+// create the play button /////////////////////////////////////////////////////
   const $play = $('button.play');
 
   $play.on('click', () => {
@@ -95,17 +143,18 @@ $(() => {
     console.log('Started');
   });
 
-// Create the reset button
+// Create the reset button ////////////////////////////////////////////////////
   const $reset = $('button.reset');
 
   $reset.on('click', () => {
+    // $reset.html('');
     // $ball.removeClass('circle');
     // $ball.prepend('');
     // $reset.html('started');
     console.log('Restarted');
   });
 
-// Create the 'ball' function
+//Create the 'ball' function///////////////////////////////////////////////////
   const $ball = $('.circle');
 
   $ball.on('click', () => {
@@ -113,16 +162,14 @@ $(() => {
     console.log('Ball moved');
   });
 
-// Block the edge so the player can't go through it.
+// Block the edge so the player can't go through it ///////////////////////////
 
-// Create Player direction function
+// Create Player direction function ///////////////////////////////////////////
 
-// Create a 'You Won!' alert/message when the player reaches the finish destination.
+// Create a 'You Won!' message when the player reaches destination ////////////
 
-// Create different difficulties
+// Create different difficulties //////////////////////////////////////////////
 
-// Create hide board function
-
-// CONSOLE LOG EVERYTHING!!! // YES! Console everthing
+// Create hide board function /////////////////////////////////////////////////
 
 });
